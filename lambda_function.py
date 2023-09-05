1f23add7e31310dc693a6d9f20fb7416465385d0
AleMacedo <alegarciamacedo@gmail.com>
2023-09-05 17:28:46 -0300

    bug fix

--- lambda_function.py
+++ lambda_function.py
@@ -5,19 +5,27 @@ logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 def lambda_handler(event, context):
+
+    data = {}
     
-    # Get the value_1 and value_2 parameters from the event object. The 
-    # runtime converts the event object to a Python dictionary
-    value_1=event['value_1']
-    value_2=event['value_2']
+    if isinstance(event, dict) == True:
+        # Get the value_1 and value_2 parameters from the event object. The 
+        # runtime converts the event object to a Python dictionary
+        value_1 = event['value_1']
+        value_2 = event['value_2']
     
-    sum = do_sum(value_1, value_2)
-    print(f"The sum is {sum}")
-        
+        sum = do_sum(value_1, value_2)
+    
+        print(f"The sum is {sum}")
+
+        # return the calculated sum as a JSON string
+        data = {"sum": sum}
+
+    else:
+        print(f"Não foi possível ler payload!")
+
     logger.info(f"CloudWatch logs group: {context.log_group_name}")
     
-    # return the calculated area as a JSON string
-    data = {"sum": sum}
     return json.dumps(data)
     
 def do_sum(value_1, value_2):
